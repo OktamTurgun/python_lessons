654c36d8b633dd204a9a6b172246dfc0a02ad19a
OktamTurgun <uktamturgunov30@gmail.com>
2025-05-05 14:39:30 +0500

    Updated Lesson 8 finished Practice 08-c

---
+++ 8-dars/08-dars.Practice/08-c.py
@@ -0,0 +1,17 @@
+# taomlar degan ro'yxat yarating va ichiga istalgan 5ta taomni kiriting
+uzbek_foods = ["osh", "norin", "somsa", "kabob", "lag'mon", "sho'rva", "qaymoq va non", "issiq non", "gurochli bo'tqa", "tuxum"]
+
+# nonushta degan yangi ro'yxatga taomlardan nusxa oling
+breakfast = uzbek_foods[:]
+
+# Yangi ro'yxatda faqat nonushtaga yeyiladigan taomlarni qoldiring, va qo'shimcha 2 ta taom qo'shing
+breakfast = breakfast[-4:]
+breakfast.extend(["botirlar bo'tqasi", 'shirin choy va non']) # 2 ta taom qo'shildi
+
+# Ikkala ro'yxatni ham (uzbek_foods va breakfast) konsolga chiqaring
+print(breakfast)
+print(uzbek_foods)
+
+# Yuqoridagi nonushta ro'yxatini o'zgarmas ro'yxatga aylantiring va nonushta[0] = "qaymoq va non" deb qiymat berib ko'ring.
+breakfast = tuple(breakfast)
+print(type(breakfast))
