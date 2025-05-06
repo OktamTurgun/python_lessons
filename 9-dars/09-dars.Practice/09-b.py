ad418c00176f01a30c5de449e1993df023b4829d
OktamTurgun <uktamturgunov30@gmail.com>
2025-05-06 06:54:13 +0500

    Update 09-b

--- 9-dars/09-dars.Practice/09-b.py
+++ 9-dars/09-dars.Practice/09-b.py
@@ -2,22 +2,22 @@
 10 dan 100 gacha bo'lgan toq sonlar ro'yxatini tuzing. 
 Ro'yxatning xar bir elementining kubini yangi qatordan konsolga chiqaring.
 """
-# for num in range(11,100,2):
-#   print(num**3)
+for num in range(11,100,2):
+  print(num**3)
 
 """
-Agar natijani ro‘yxat shaklida saqlash kerak bo‘lsa, list comprehension ishlatish mumkin:
+Agar natijani ro'yxat shaklida saqlash kerak bo'lsa, list comprehension ishlatish mumkin:
 """
-# cubes = [num**3 for num in range(11, 100, 2)]
-# print(cubes)
+cubes = [num**3 for num in range(11, 100, 2)]
+print(cubes)
 
 """
 Agar natijani birma-bir chiqarish kerak bo'lsa va ro'yxat saqlash 
 shart bo'lmasa, generator ishlatish yaxshi:
 """
-# cubes = [num**3 for num in range(11, 100, 2)]
-# for cube in cubes:
-#   print(cube)
+cubes = [num**3 for num in range(11, 100, 2)]
+for cube in cubes:
+  print(cube)
 
 """
 Agar CPU hisoblashda qiyinchilik bo'lsa, num * num * num 
