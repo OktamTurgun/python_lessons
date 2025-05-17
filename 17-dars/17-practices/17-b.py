ba283af8d696ea1daf074ba5bd2c4e166f74117e
OktamTurgun <uktamturgunov30@gmail.com>
2025-05-17 14:52:04 +0500

    Update 17-b.py

--- 17-dars/17-practices/17-b.py
+++ 17-dars/17-practices/17-b.py
@@ -1,39 +1,39 @@
 """
 Muzeyga chipta narhi foydalanuvchining yoshiga bog'liq: 7 dan yoshlarga - 2000 so'm, 7-18 gacha 3000 so'm, 18-65 gacha 10000 so'm, 65 dan kattalarga bepul. Shunday while tsikl yozingki, dastur foydalanuvchi yoshini so'rasin va chipta narhini chiqarsin. Foydalanuvchi exit yoki quit deb yozganda dastur to'xtasin (ikkita shartni ham tekshiring).
 """
-savol = "Yoshingizni kiriting: "
+# savol = "Yoshingizni kiriting: "
 
-while True:
-  qiymat = input(savol).strip()  
-  # if qiymat.lower() == 'exit' or qiymat.lower() == 'quite':
-  if qiymat.lower() in ('exit', 'quite', 'stop', 'quit'): # in operatori yaxshiroq variant
-    print("Dastur tugadi")
-    break
+# while True:
+#   qiymat = input(savol).strip()  
+#   # if qiymat.lower() == 'exit' or qiymat.lower() == 'quite':
+#   if qiymat.lower() in ('exit', 'quite', 'stop', 'quit'): # in operatori yaxshiroq variant
+#     print("Dastur tugadi")
+#     break
   
-  try:
-    age = int(qiymat)
-    if age<0 or age>100:
-      raise ValueError("Yosh 0 bilabn 100 oralig'ida bo'lishi kerak!")
-  except ValueError as e:
-    if "invalit literal" in str(e).lower():
-      print("Iltimos, faqat raqam kiriting!: ")
-    else:
-      print(e)
-    continue
+#   try:
+#     age = int(qiymat)
+#     if age<0 or age>100:
+#       raise ValueError("Yosh 0 bilan 100 oralig'ida bo'lishi kerak!")
+#   except ValueError as e:
+#     if "invalit literal" in str(e).lower():
+#       print("Iltimos, faqat raqam kiriting!: ")
+#     else:
+#       print(e)
+#     continue
   
-  if age<7 or age>65:
-    price = 0
-  elif 7 <=age<= 18:
-    price = 15000
-  else: 
-    price = 25000
+#   if age<7 or age>65:
+#     price = 0
+#   elif 7 <=age<= 18:
+#     price = 15000
+#   else: 
+#     price = 25000
     
-  # if price == 0:
-  #   print("Sizga kirish bepul.") 
-  # else:
-  #   print(f"Sizga kirish {price} so'm.")
-  # f-string boaln xam narhlarni chiqarish mumkin
-  print(f"Sizga kirish {'bepul' if price == 0 else f'{price} so\'m'}.")
+#   # if price == 0:
+#   #   print("Sizga kirish bepul.") 
+#   # else:
+#   #   print(f"Sizga kirish {price} so'm.")
+#   # f-string boaln xam narhlarni chiqarish mumkin
+#   print(f"Sizga kirish {'bepul' if price == 0 else f'{price} so\'m'}.")
 
 '''Example 2'''   
 # savol = "Yoshingizni kiriting: "
@@ -56,4 +56,48 @@ while True:
 #         print("Sizga chipta bepul")
 #     else:
 #         print(f"Chipta {narh} so'm")
+
+'''Example 3'''   
+# savol = "Yoshingizni kiriting: "
+# ishora = True
+
+# while ishora:
+#     qiymat = input(savol)
+#     if qiymat == 'exit' or qiymat == 'quit':
+#       print("Dastur to'xtatildi!")
+#       ishora = False
+      
+#     yosh = int(qiymat)
     
+#     if yosh<7 or yosh > 65:
+#         narh = 0
+#     elif 7<=yosh<18:
+#         narh = 12000
+#     else:
+#         narh = 25000
+  
+#     if narh==0:
+#         print("Sizga chipta bepul")
+#     else:
+#         print(f"Chipta {narh} so'm")
+        
+'''Example 4 Loopni shart orqali to'xtatish'''   
+savol = "Yoshingizni kiriting: "
+savol += "(Chiqish uchun 'exit' deb yozing) "
+ishora = ''
+
+while ishora !='exit':
+    qiymat = input(savol)
+    if qiymat != 'exit':
+      yosh = int(qiymat)
+    
+      if yosh<7 or yosh > 65:
+          narh = 0
+      elif 7<=yosh<18:
+          narh = 12000
+      else:
+          narh = 25000
+      print(f"Chipta {narh} so'm")
+    else:
+      print("Dastur tugadi")
+          
