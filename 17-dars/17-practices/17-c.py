a2d895176f980ee4448288bec3168a14326af651
OktamTurgun <uktamturgunov30@gmail.com>
2025-05-18 09:18:08 +0500

    Added new examples for Lesson17/17-c.py

--- 17-dars/17-practices/17-c.py
+++ 17-dars/17-practices/17-c.py
@@ -19,22 +19,73 @@ qolmoqda. Xatolarni to'g'rilay olasizmi?
 #         print(f"{qiymat} ning ildizi {ildiz} ga teng")
         
 '''try-except yordamida tekshiramiz'''
-savol = "Kiritilgan sonning ildizini qaytaruvchi dastur.\n"
-savol += "Musbat son kiriting: "
-savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+# savol = "Kiritilgan sonning ildizini qaytaruvchi dastur.\n"
+# savol += "Musbat son kiriting: "
+# savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
 
-while True:
-    qiymat = input(savol)
-    if qiymat == 'exit':
-      print("Dastur tugatildi!")
-      break
-    try:
-      qiymat = float(qiymat)
-      if qiymat < 0:
-        print("Musbat son kiriting!")
-        continue
-      ildiz = qiymat ** 0.5
-      print(f"{qiymat} ning ildizi {ildiz} ga teng")
-    except ValueError:
-      print("Iltomos, musbat son kiriting yoki 'exit' deb yozing!")  
+# while True:
+#     qiymat = input(savol)
+#     if qiymat == 'exit':
+#       print("Dastur tugatildi!")
+#       break
+#     try:
+#       qiymat = float(qiymat)
+#       if qiymat < 0:
+#         print("Musbat son kiriting!")
+#         continue
+#       ildiz = qiymat ** 0.5
+#       print(f"{qiymat} ning ildizi {ildiz} ga teng")
+#     except ValueError:
+#       print("Iltomos, musbat son kiriting yoki 'exit' deb yozing!")  
       
+"""
+Parol tekshiruvchi dastur tuzing. 3 ta urunish berilsin.
+"""
+login = "python123"
+attempts = 3
+
+while attempts > 0:
+  qiymat = input("Parol kiriting: ")
+  if qiymat == login:
+    print("Xush kelibsiz!")
+    break
+  attempts -= 1
+  print(f"Noto'g'ri login! {attempts} ta urunish qoldi")
+else:
+  print("Hisobingiz bolklandi")
+  
+''' 1dan n gacha bo'lgan toq sonlarni chiqaring. '''
+n = int(input("N sonini kiriting: "))
+i = 1
+while i <= n:
+  if i % 2 != 0:
+    print(i, end=" ")
+  i += 1 
+  
+''' faktorial hisoblash '''
+n = int(input("Son kiriting: "))
+factorial = 1
+i = 1
+while i <= n:
+  factorial *= i
+  i += 1
+print(f"{n}! = {factorial}")
+
+''' Sonni teskari chiqarish '''
+num = int(input("Son kiriting: "))
+teskari = 0
+while num > 0:
+  teskari = teskari * 10 + num % 10
+  num = num // 10
+print(f"Teskari son: {teskari}")
+
+''' Tub sonini tekshirish '''
+n = int(input("Son kiriting: "))
+i = 2
+tub_son = True
+while i < n:
+  if n % i == 0:
+    tub_son = False
+    break
+  i += 1
+print(f"{n} tub son." if tub_son else f"{n} tub son emas")
