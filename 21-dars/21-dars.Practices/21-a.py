5d079fc59a4119941affdd87153af97fca9c8a0e
Uktam <uktamturgunov30@gmail.com>
2025-05-24 10:21:30 +0500

    Create new practice file 21-a.py

---
+++ 21-dars/21-dars.Practices/21-a.py
@@ -0,0 +1,38 @@
+'''
+Matnlardan iborat ro'yxat qabul qilib, ro'yxatdagi har bir
+matnning birinchi harfini katta harfga o'zgatiruvchi funksiya yozing. 
+'''
+def capitalize_words(words):
+    capitalized_words = []
+    for word in words:
+        capitalized_words.append(word.capitalize())
+    return capitalized_words
+  
+fruits = ['olma', 'banan', 'apelsin']
+capitalized_fruits = capitalize_words(fruits) 
+print(capitalized_fruits) # ['Olma', 'Banan', 'Apelsin']
+
+''''''
+def katta_harf(royxat):
+    yangi = royxat.copy()  # asl ro'yxatdan nusxa
+    for i in range(len(yangi)):
+        yangi[i] = yangi[i].capitalize()
+    return yangi
+names = ['john', 'alex', 'micheal']
+print(katta_harf(names))      # ['John', 'Alex', 'Micheal']
+print(names)               # ['john', 'alex', 'micheal']
+
+''''''  
+def katta_harf(royxat):
+    return [x.capitalize() for x in royxat]
+  
+clothes = ['kofta', 'shim', 'kurtka']
+print(katta_harf(clothes))      # ['Kofta', 'Shim', 'Kurtka']
+
+''''''
+def capitalize_words(words):
+    return [word.capitalize() for word in words]  
+  
+cars = ['toyota', 'bmw', 'mercedes']
+capitalized_cars = capitalize_words(cars)   
+print(capitalized_cars) # ['Toyota', 'Bmw', 'Mercedes']
