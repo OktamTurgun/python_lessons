d6546a1247f4efac61e40e62a0b0d10a4e3298bc
Uktam <uktamturgunov30@gmail.com>
2025-05-24 10:44:34 +0500

    Added new exercise, 21-a.py

--- 21-dars/21-dars.Practices/21-a.py
+++ 21-dars/21-dars.Practices/21-a.py
@@ -2,6 +2,18 @@
 Matnlardan iborat ro'yxat qabul qilib, ro'yxatdagi har bir
 matnning birinchi harfini katta harfga o'zgatiruvchi funksiya yozing. 
 '''
+def katta_harf(royxat):
+    """Ro'yxatdagi har bir matnning birinchi harfini katta harfga o'zgartiradi"""
+    # ro'yxatni o'zgartiradi   
+    for i in range(len(royxat)):
+      royxat[i] = royxat[i].title()
+    return royxat
+ismlar = ['ilhom', 'olim', 'komil', 'murod', 'alisher']
+names = katta_harf(ismlar)
+print(names) # ['Ilhom', 'Olim', 'Komil', 'Murod', 'Alisher']
+print(ismlar) # ['ilhom', 'olim', 'komil', 'murod', 'alisher']
+
+''''''
 def capitalize_words(words):
   """Ro'yxatdagi har bir matnning birinchi harfini katta harfga o'zgartiradi"""
   # royxatni o'zgartirmaydi
@@ -13,6 +25,7 @@ def capitalize_words(words):
 fruits = ['olma', 'banan', 'apelsin']
 capitalized_fruits = capitalize_words(fruits) 
 print(capitalized_fruits) # ['Olma', 'Banan', 'Apelsin']
+print(fruits) # ['olma', 'banan', 'apelsin']
 
 ''''''
 def katta_harf(royxat):
@@ -44,3 +57,4 @@ def capitalize_words(words):
 cars = ['toyota', 'bmw', 'mercedes']
 capitalized_cars = capitalize_words(cars)   
 print(capitalized_cars) # ['Toyota', 'Bmw', 'Mercedes']
+print(cars) # ['toyota', 'bmw', 'mercedes']
