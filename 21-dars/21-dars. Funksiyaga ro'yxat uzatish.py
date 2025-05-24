f26c859fffa3667a6641d6ee11d528193651616e
Uktam <uktamturgunov30@gmail.com>
2025-05-24 10:28:35 +0500

    Added comments

--- 21-dars/21-dars. Funksiyaga ro'yxat uzatish.py
+++ 21-dars/21-dars. Funksiyaga ro'yxat uzatish.py
@@ -6,6 +6,8 @@ Created on Fri May 23 20:35:42 2025
 @author: uktam
 """
 def bahola(ismlar):
+    # Talabalar ro'yxatini qabul qilib, baholarni so'raydi
+    # va natijani lug'at ko'rinishida qaytaradi
     baholar = {}
     while ismlar:
         ism = ismlar.pop()
@@ -23,6 +25,7 @@ for ism, baho in baholar.items():
 
 ''''''
 def royxatni_korsat(royxat):
+    # Ro'yxatni qabul qilib, har bir elementini chiqaradi
     for element in royxat:
         print(element)
         
@@ -36,6 +39,8 @@ Funksiya ichida ro'yxatga o'zgartirish kiritilsa, asl ro'yxat ham o'zgaradi
 '''
 
 def qoshish(royxat):
+    # Ro'yxatga yangi element qo'shadi
+    # va asl ro'yxatni o'zgartiradi
     royxat.append("new element")
     
 numbers = [1, 2, 3]
@@ -49,6 +54,8 @@ Ro'yxatni nusxalash (Avoiding Side Effects)
 Agar asl ro'yxat o'zgarishini istamasangiz, nusxa olishingiz kerak.
 '''
 def qoshish_yangi(royxat):
+    # Ro'yxatga yangi element qo'shadi
+    # va yangi ro'yxatni qaytaradi
     yangi_royxat = royxat.copy()
     yangi_royxat.append("new element")
     return yangi_royxat
@@ -61,7 +68,8 @@ print(yangi_royxat) # [1, 2, 3, 'new element']
 Ro'yxatni parametr sifatida ishlatish.
 Funksiyaga bir nechta ro'yxat uzatish mumkin.
 '''
-def birlashtir(list1, list2):
+def birlashtir(list1, list2):   
+    """Ikkita ro'yxatni birlashtiradi va yangi ro'yxatni qaytaradi"""
     return list1 + list2
 
 a = ['a', 'b', 'c']
@@ -84,6 +92,7 @@ Argument sifatida uzatish: Ro'yxat nomini to'g'ri berish
 ''''''
 # Ro'yxat lementlarini kvadratga oshirish
 def kvadrat(list):
+    """Ro'yxatdagi har bir elementni kvadratga oshiradi va asl ro'yxatni o'zgartiradi"""
     for i in range(len(list)):
         #list[i] **= 2 # shortcut for list[i] = list[i] ** 2
         list[i] = list[i] ** 2
@@ -95,6 +104,7 @@ print(nums) # [1, 4, 9]
 ''''''
 # Ro'yxatdagi juft sonlarni ajratish
 def juft_sanash(royxat):
+    """Ro'yxatdagi juft sonlarni ajratadi va yangi ro'yxatni qaytaradi"""
     juftlar = []
     for son in royxat:
         if son % 2 == 0:
@@ -105,6 +115,8 @@ print(juft_sanash([1, 2, 3, 4, 5, 6])) # [2, 4, 6]
 
 ''''''
 def add(royxat):
+    """Ro'yxatdagi har bir elementga 5 qo'shadi va yangi ro'yxatni qaytaradi"""
+    # royxatni o'zgartirmaydi
     new_numbers = []
     for x in royxat:
         new_numbers.append(x + 5)
@@ -117,6 +129,8 @@ print(f"Yangi ro'yaxat: {new_numbers}")
 
 ''''''
 def add_five(royxat):
+    """Ro'yxatdagi har bir elementga 5 qo'shadi va yangi ro'yxatni qaytaradi"""
+    # royxatni o'zgartirmaydi
     return [x + 5 for x in numbers]
 
 numbers = [10, 20, 30]
@@ -125,7 +139,9 @@ print(f"Eski ro'yxat: {numbers}")
 print(f"Yangi ro'yxat: {new_numbers}")
 
 ''''''
-def add_five(royxat):
+def add_five(royxat):   
+    """Ro'yxatdagi har bir elementga 5 qo'shadi va yangi ro'yxatni qaytaradi"""
+    # royxatni o'zgartirmaydi   
     yangi = royxat.copy()  # asl ro'yxatdan nusxa
     for i in range(len(yangi)):
         yangi[i] += 5
