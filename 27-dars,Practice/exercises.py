4fb9fd70a25cc3573ca6205e8af7fa4f5f74ac1e
Uktam <uktamturgunov30@gmail.com>
2025-06-06 16:55:59 +0500

    Added new exercises file exercises.py

--- 27-dars,Practice/exercises.py
+++ 27-dars,Practice/exercises.py
@@ -9,12 +9,22 @@ from transliterate import to_cyrillic, to_latin
 import telebot
 print("Kutubxona muvaffaqiyatli yuklandi!")
 
-# TOKEN = '7891811969:AAEBAVEECCwJYvzYrke0inFR90V4WM8wOgs'
-bot = telebot.TeleBot("7891811969:AAEBAVEECCwJYvzYrke0inFR90V4WM8wOgs", parse_mode=None)
+TOKEN = '7891811969:AAEBAVEECCwJYvzYrke0inFR90V4WM8wOgs'
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Asssalomu aleykum, Xush kelibsiz!")
+  javob = "Asssalomu aleykum, Xush kelibsiz!"
+  javob += "\nMatn kiriting!"
+  bot.reply_to(message, javob)
+  
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+  msg = message.text
+  javob = lambda msg: to_cyrillic(msg) if msg.isascii() else to_latin(msg) 
+  
+  bot.reply_to(message, javob(msg))
+	
  
 bot.infinity_polling()
  
