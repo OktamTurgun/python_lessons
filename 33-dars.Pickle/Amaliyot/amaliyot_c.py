5e5d6ea0917de2161cc68a93910b693247babb78
Uktam <uktamturgunov30@gmail.com>
2025-06-21 11:42:09 +0500

    Create a file named amaliyot_c.py, and convert the text inside the file pi_million_digits.txt to a float data type. Then, save it to a new file using the pickle module.

---
+++ 33-dars.Pickle/Amaliyot/amaliyot_c.py
@@ -0,0 +1,30 @@
+"""
+Fayl ichidagi matnni float ma'lumot turiga o'tkazing va pickle yordamida yangi faylga saqlang.
+"""
+import pickle
+
+with open(r"C:\\Users\\User\\Documents\\GitHub\\python_lessons\\python_lessons\\33-dars.Pickle\\Practice\\pi_million_digits.txt") as file:
+    pi = file.read().strip().replace("\n", "").replace(" ", "")  # Fayldagi matnni o'qib olamiz
+
+# Tug'ilgan kunim pi da bormi?
+bdate = "17072017" # 17 Iyul, 2017-yil
+if bdate in pi:
+  print(f"Tug'ilgan kunim {(bdate)} π sonida mavjud.")
+else:
+  print(f"Tug'ilgan kunim {(bdate)} π sonida mavjud emas.")
+print(bdate in pi) # True or False
+
+'''
+# pi = float(pi) # matnni float (o'nlik) songa o'tkazamiz
+
+# with open('amaliyot/pi_float.dat','wb') as file:
+#     pickle.dump(pi,file)
+'''
+pi_float = float(pi) # matnni float (o'nlik) songa o'tkazamiz
+with open('pi_float.dat', 'wb') as file:
+  pickle.dump(pi_float, file)
+  
+# Faylni o'qish
+with open('pi_float.dat', 'rb') as file:
+    pi_from_file = pickle.load(file)
+print(f"Fayldan o'qilgan π soni: {pi_from_file}") # Fayldan o'qilgan π soni: 3.141592653589793
