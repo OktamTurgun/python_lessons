5964683456a4f36877882c89e5e8c1bc656717fa
Uktam <uktamturgunov30@gmail.com>
2025-06-29 11:40:55 +0500

    Update statistic_library.py

--- 38-dars/statistic_library/statistic_library.py
+++ 38-dars/statistic_library/statistic_library.py
@@ -37,3 +37,15 @@ print("Eng ko‘p baho (moda):", stats.mode(baholar))
 # Amaliy mashq: Ijtimoiy tarmoqlardagi postlar ustida statistik hisob
 post_likes = [100, 150, 200, 150, 300, 250, 200, 150]
 print("Postlar o‘rtacha like soni:", stats.mean(post_likes))
+
+# Amaliy mashq: Talabalar yoshlarining statistikasi
+talabalar_yoshi = [19, 20, 21, 20, 22, 21, 20, 19, 21, 22]
+print("Talabalar o‘rtacha yoshi:", stats.mean(talabalar_yoshi))
+print("Talabalar median yoshi:", stats.median(talabalar_yoshi))
+print("Talabalar yoshidagi moda:", stats.mode(talabalar_yoshi))
+
+# Amaliy mashq: Mahsulot sotuvlari statistikasi
+sotuvlar = [50, 60, 55, 70, 65, 60, 55, 60]
+print("O‘rtacha sotuvlar soni:", stats.mean(sotuvlar))
+print("Sotuvlar dispersiyasi:", stats.variance(sotuvlar))
+print("Sotuvlar standart og‘ishi:", stats.stdev(sotuvlar))
