fcd98c9c1bc66c2d6f77b6a508a35ff6869bc1ae
Uktam <uktamturgunov30@gmail.com>
2025-07-06 11:43:34 +0500

    Added new practice file "ism_yosh.py" for Practice/Basic&Syntax

---
+++ Practice/Basic & Syntaxni mustaxkamlash/ism_yosh.py
@@ -0,0 +1,12 @@
+"""
+Mavzu: Practice. Basic & Syntaxni mustaxkamlash
+Muallif: Uktam Turgunov
+Sana: 2025-07-06
+Vazifa:
+Foydalanuvchidan ismingiz va yoshingizni kiriting va shunday xabar chiqaring:
+"""
+ism = input('Ismingizni kiriting: ')
+yosh = int(input("yoshingizni kiriting: "))
+
+print(
+    f"Salom {ism.title()}, Siz {2025-yosh}-yilda tug'ilgansiz, Yoshingiz {yosh} da")
